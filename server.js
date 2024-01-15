ffcaff16b7f74889a67316e4d9c4f45e2cb552d5
Novita H <novitahamilton@gmail.com>
2024-01-15 13:58:13 -0800

    Add nodemon and add GET:id function
    (1) Install nodemon as devDependencies
    (2) Add "dev" on package.json scripts
    (3) Add GET:id functionality in server.js
    (4) Update endpoint name '/api/currency' to 'api/currencies'
    (5) Add GET currencies and GET currencies/id calls in test.rest file

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express')  // We import the express application
-const cors = require('cors') // Necessary for localhost
-const app = express() // Creates an express application in app
+const express = require('express'); // We import the express application
+const cors = require('cors'); // Necessary for localhost
+const app = express(); // Creates an express application in app
 
 /**
  * Initial application setup
  * We need to use cors so we can connect to a localhost later
  * We need express.json so we can receive requests with JSON data attached
  */
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 /**
  * DATA STORAGE
  * We're using a local variable 'currencies' to store our data: a list of currency objects
  * Each object represents a 'currency', and contains the following fields
- * id: a number, 
+ * id: a number,
  * currencyCode: a string, three letters (see https://www.iban.com/currency-codes as reference)
  * country: a string, the name of the country
  * conversionRate: the amount, in that currency, required to equal 1 Canadian dollar
@@ -23,17 +22,17 @@ app.use(express.json())
 let currencies = [
   {
     id: 1,
-    currencyCode: "CDN",
-    country: "Canada",
-    conversionRate: 1
+    currencyCode: 'CDN',
+    country: 'Canada',
+    conversionRate: 1,
   },
   {
     id: 2,
-    currencyCode: "USD",
-    country: "United States of America",
-    conversionRate: 0.75
-  }
-]
+    currencyCode: 'USD',
+    country: 'United States of America',
+    conversionRate: 0.75,
+  },
+];
 
 /**
  * TESTING Endpoint (Completed)
@@ -41,27 +40,28 @@ let currencies = [
  * @responds with the string 'Hello World!'
  */
 app.get('/', (request, response) => {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
 
 /**
  * TODO: GET Endpoint
  * @receives a get request to the URL: http://localhost:3001/api/currency/
  * @responds with returning the data as a JSON
  */
-app.get('/api/currency/', (request, response) => {
-  response.json(currencies)
-})
+app.get('/api/currencies/', (request, response) => {
+  response.json(currencies);
+});
 
 /**
  * TODO: GET:id Endpoint
- * @receives a get request to the URL: http://localhost:3001/api/currency/:id
+ * @receives a get request to the URL: http://localhost:3001/api/currencies/:id
  * @responds with returning specific data as a JSON
  */
-app.get('...', (request, response) => {
-
-
-})
+app.get('/api/currencies/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const currency = currencies.find((currency) => currency.id === id);
+  response.json(currency);
+});
 
 /**
  * TODO: POST Endpoint
@@ -69,10 +69,7 @@ app.get('...', (request, response) => {
  * with data object enclosed
  * @responds by returning the newly created resource
  */
-app.post('...', (request, response) => {
-
-
-})
+app.post('...', (request, response) => {});
 
 /**
  * TODO: PUT:id endpoint
@@ -81,21 +78,16 @@ app.post('...', (request, response) => {
  * Hint: updates the currency with the new conversion rate
  * @responds by returning the newly updated resource
  */
-app.put('...', (request, response) => {
-  
-})
+app.put('...', (request, response) => {});
 
 /**
  * TODO: DELETE:id Endpoint
  * @receives a delete request to the URL: http://localhost:3001/api/currency/:id,
  * @responds by returning a status code of 204
  */
-app.post('...', (request, response) => {
+app.post('...', (request, response) => {});
 
-
-})
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`)
-})
+  console.log(`Server running on port: ${PORT}`);
+});
