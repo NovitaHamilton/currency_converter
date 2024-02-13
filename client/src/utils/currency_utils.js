7c42261712c85b3d0f62912fee5a80ed0facb7d1
Novita H <novitahamilton@gmail.com>
2024-02-12 16:43:01 -0800

    Part 1
    1. Create tests directory, and add currency.test.js file from Part1 starter file here
    2.Create utils directory and add currency_utils.js filefrom Part1 starter file here
    3. Install jest and add"test" command  the to the package.json within the scripts
    4. Implement the tests below in currency.test.js:
     ✓ CDN to GBP conversion
      ✓ CDN to USD conversion
      ✓ USD to GBP conversion
      ✓ GBP to CAD conversion
    5. Implement convertCurrency function with 4 conditions:
      ✓ CDN to CDN conversion
      ✓ CDN to non-CDN conversion
      ✓ Non-CDN to CDN conversion
      ✓ Non-CDN to non-CDN conversion.
    6. Run the test with 5 passed (all passed)

---
+++ client/src/utils/currency_utils.js
@@ -0,0 +1,34 @@
+/**
+ * This file is meant to be where you will complete the utility function below, for performing a conversion of some amount
+ * to another currency.
+ */
+
+/**
+ * TODO:
+ * @receives two currency objects, currencyA and currencyB, as well as an integer-amount of cash in currencyA
+ * @performs a currency conversion between integer amount of currencyA to an integer amount of currencyB
+ * @returns an integer
+ */
+const convertCurrency = (currencyA, currencyB, amount) => {
+  // CAD to CAD conversion
+  if (currencyA === currencyB) {
+    return amount;
+    // CAD to non-CAD
+  } else if (currencyA === 'CAD') {
+    return parseFloat(amount * currencyB.conversionRate).toFixed(2);
+  }
+  // Non-CAD to CAD
+  else if (currencyB === 'CAD') {
+    return parseFloat((amount / currencyA.conversionRate).toFixed(2));
+  }
+  // Non-CAD to CAD
+  else {
+    return parseFloat(
+      ((amount / currencyA.conversionRate) * currencyB.conversionRate).toFixed(
+        2
+      )
+    );
+  }
+};
+
+module.exports = convertCurrency;
