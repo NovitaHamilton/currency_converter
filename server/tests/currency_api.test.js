b49ceafe661b9d87b3bb8a561abf9f5f24de9fa5
Novita H <novitahamilton@gmail.com>
2024-02-23 14:33:43 -0800

    Update test helper file
    1. Install jest and supertest in server directory
    2. Add import path to Currency model in the test_helper file

--- server/tests/currency_api.test.js
+++ server/tests/currency_api.test.js
@@ -1,20 +1,20 @@
 /**
  * Necessary imports, make sure you have these packages installed in your server directory
  */
-const supertest = require('supertest')
-const sequelize = require('...') // Provide a path to your config.js or database.js file, wherever you export that sequelize
-const helper = require('./test_helper')
-const server = require('...') // Provide a path to your server.js file, or wherever you are starting your server and add your endpoints via router
-const api = supertest(server) // Creates a test api that will send requests where we want them to be sent
+const supertest = require('supertest');
+const sequelize = require('...'); // Provide a path to your config.js or database.js file, wherever you export that sequelize
+const helper = require('./test_helper');
+const server = require('...'); // Provide a path to your server.js file, or wherever you are starting your server and add your endpoints via router
+const api = supertest(server); // Creates a test api that will send requests where we want them to be sent
 
 beforeEach(async () => {
   // Setup currencies table (if not already setup)
-  await helper.init()
+  await helper.init();
 
   // Clear data and load new entries for tests
-  await helper.clearData()
-  await helper.load()
-})
+  await helper.clearData();
+  await helper.load();
+});
 
 describe('GET tests', () => {
   /**
@@ -23,9 +23,9 @@ describe('GET tests', () => {
    * we added the two blogs in the 'beforeEach' setup phase
    */
   test('we have 2 currencies at the start', async () => {
-    const response = await api.get('/api/currencies')
-    expect(response.body).toHaveLength(2)
-  })
+    const response = await api.get('/api/currencies');
+    expect(response.body).toHaveLength(2);
+  });
 
   /**
    * Completed:
@@ -36,53 +36,48 @@ describe('GET tests', () => {
    * such as time of last update and so on
    */
   test('getting a specific currency', async () => {
-    const canadianCurrency = helper.initialCurrencies[0]
-    const getId = canadianCurrency.id
+    const canadianCurrency = helper.initialCurrencies[0];
+    const getId = canadianCurrency.id;
 
     // Verify that we get the same currency
-    const response = await api
-      .get(`/api/currencies/${getId}`)
-      .expect(200)
+    const response = await api.get(`/api/currencies/${getId}`).expect(200);
 
     // As stated above, we will compare the conversionRate and currencyCode
-    const currencyReceived = response.body
-    expect(canadianCurrency.conversionRate).toEqual(currencyReceived.conversionRate)
-    expect(canadianCurrency.currencyCode).toEqual(currencyReceived.currencyCode)
-  })
-})
+    const currencyReceived = response.body;
+    expect(canadianCurrency.conversionRate).toEqual(
+      currencyReceived.conversionRate
+    );
+    expect(canadianCurrency.currencyCode).toEqual(
+      currencyReceived.currencyCode
+    );
+  });
+});
 
 /**
  * The tests for POST, PUT, and DELETE are left un-implemented, and you will have to complete them
  * All the helper functions have been provided, and the examples as well are sufficient
  * You will need to do some reading on supertest documentation as well
- * 
+ *
  * IMPORTANT: You are only working with currencies, we removed the countries connection to make it a bit simpler
  */
 
 describe('POST tests', () => {
   // Add a currency, and verify that a currency is added to our database
-  test('adding a currency', () => {
-
-  })
-})
+  test('adding a currency', () => {});
+});
 
 describe('PUT tests', () => {
   // Update a currency, and verify that a currency has been updated
-  test('adding a currency', () => {
-
-  })
-})
+  test('adding a currency', () => {});
+});
 
 describe('DELETE tests', () => {
   // Delete a currency, and verify that a currency has been deleted
-  test('adding a currency', () => {
-
-  })
-})
+  test('adding a currency', () => {});
+});
 
 afterAll(async () => {
   // Closes connection after all tests run
-  server.close()
-  await sequelize.close()
-})
-
+  server.close();
+  await sequelize.close();
+});
