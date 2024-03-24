bb56e793e507c492d58b2925a366c5775e498354
Novita H <novitahamilton@gmail.com>
2024-03-24 08:52:59 -0700

    Update proxy target

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Adjust this based on your server configuration
+        target: 'https://currency-converter-1.onrender.com', // Adjust this based on your server configuration
         changeOrigin: true,
       },
     },
